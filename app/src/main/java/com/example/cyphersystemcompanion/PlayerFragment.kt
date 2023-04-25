6ba086e4c00b6f21b6c7fe57e616cd2dac9623ce
scheysej <scheysej@udmercy.edu>
2023-04-25 00:51:03 -0400

    Updated the look of the firebase info

--- app/src/main/java/com/example/cyphersystemcompanion/PlayerFragment.kt
+++ app/src/main/java/com/example/cyphersystemcompanion/PlayerFragment.kt
@@ -1,27 +1,37 @@
 package com.example.cyphersystemcompanion
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
 import android.widget.Toolbar
 import com.example.cyphersystemcompanion.databinding.FragmentDmBinding
 import com.example.cyphersystemcompanion.databinding.FragmentPlayerBinding
 
 class PlayerFragment : Fragment() {
     private var _binding: FragmentPlayerBinding? = null
-
     private val binding get() = _binding!!
-
-    override fun onCreate(savedInstanceState: Bundle?){
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var listView: ListView
+    private val data = listOf(
+        "Choose Your Type",
+        "Choose Your Flavor",
+        "Choose Your Descriptor",
+        "Choose Your Focus",
+        "Choose Your Abilities",
+        "Pick A Name",
+        "Make Some Character Art!!"
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View?{
+
         _binding = FragmentPlayerBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
@@ -31,4 +41,5 @@ class PlayerFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
 }
